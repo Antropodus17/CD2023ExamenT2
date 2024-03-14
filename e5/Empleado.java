58f28422eab790f6657fd56d123dab3569c7443a
Antropodus17 <a23sergiopn@iessanclemente.net>
2024-03-14 11:17:00 +0100

    Ejercicio e5

---
+++ e5/Empleado.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+public class Empleado {
+
+	String nombre;
+	int sueldo;
+	
+	public Empleado(String string, int i) {
+		this.nombre=string;
+		this.sueldo=i;
+	}
+
+	
+
+}
